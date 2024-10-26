33ec7718458311611ed1d65b79a02af589d74616
Jonathan Balat <engrjonathanbalat@gmail.com>
2024-10-26 12:35:09 +0800

    UPdated test to include fixture; Added improved fibo method that uses LRU caching

--- Python/Pytest/src/Math_Class.py
+++ Python/Pytest/src/Math_Class.py
@@ -1,9 +1,22 @@
+from functools import lru_cache
+
 
 class MathClass(object):
 
-    def fibbonacci_sequence(self, n):
+    def fibbonacci_sequence_prototype(self, n):
         if n > 1:
-            val = self.fibbonacci_sequence(n - 1) + self.fibbonacci_sequence(n - 2)
+            val = self.fibbonacci_sequence_prototype(n - 1) + self.fibbonacci_sequence_prototype(n - 2)
+        elif n > 0:
+            val = 1
+        else:
+            val = 0
+
+        return val
+
+    @lru_cache
+    def fibbonacci_sequence_improved(self, n):
+        if n > 1:
+            val = self.fibbonacci_sequence_improved(n - 1) + self.fibbonacci_sequence_improved(n - 2)
         elif n > 0:
             val = 1
         else:
@@ -15,4 +28,4 @@ class MathClass(object):
 if __name__ == '__main__':
     mc = MathClass()
 
-    print(f"Result = {mc.fibbonacci_sequence(3)}")
+    print(f"Result = {mc.fibbonacci_sequence_prototype(3)}")
--- Python/Pytest/test/test_math_class.py
+++ Python/Pytest/test/test_math_class.py
@@ -1,18 +1,23 @@
 import pytest
-import logging
 from src.Math_Class import MathClass
 
+FIBO_SAMPLE_DATA = [(0, 0), (1, 1), (2, 1), (10, 55), (12, 144), (19, 4181), (20, 6765), (30, 832040)]
+@pytest.fixture
+def MClass():
+    return MathClass()
 
-def test_class_instantiation():
-    MC = MathClass()
 
-    assert MC is not None
+def test_class_instantiation(MClass):
+    assert MClass is not None
 
 
-@pytest.mark.parametrize("test_num, test_val", [(0,0), (1,1), (2,1), (12, 144)])
-def test_class_fibo(test_num, test_val):
-    MC = MathClass()
+@pytest.mark.parametrize("test_num, test_val", FIBO_SAMPLE_DATA)
+def test_class_fibo(MClass, test_num, test_val):
+    assert MClass.fibbonacci_sequence_prototype(test_num) == test_val
 
-    assert MC.fibbonacci_sequence(test_num) == test_val
+
+@pytest.mark.parametrize("test_num, test_val", FIBO_SAMPLE_DATA)
+def test_class_fibo_improved(MClass, test_num, test_val):
+    assert MClass.fibbonacci_sequence_improved(test_num) == test_val
 
 
